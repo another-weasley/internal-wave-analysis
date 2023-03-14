5038e4932c2f94aea789497fd0a78b0c219b5f99
Anna Kozlovskaya <annhelkoz@gmail.com>
2023-03-14 16:09:31 +0300

    fix salinity and potential density, add salinity profile

--- SeaPoint.py
+++ SeaPoint.py
@@ -5,6 +5,7 @@ import matplotlib
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 import gsw
+import math
 
 
 class SeaPoint:
@@ -23,10 +24,28 @@ class SeaPoint:
         self.longitude = 131
         self.height = mat[height_header][0][0]
         # print(mat["cS09"])
-        self.salinity = {5: 33.035, 10: 33.292, 15: 33.363, 20: 33.435, 25: 33.492,
+
+        self.salinity_woa = {5: 33.035, 10: 33.292, 15: 33.363, 20: 33.435, 25: 33.492,
                          30: 33.532, 35: 33.590, 40: 33.641, 45: 33.712, 50: 33.761,
                          55: 33.830, 60: 33.868, 65: 33.902, 70: 33.929, 75: 33.954,
                          80: 33.983, 85: 33.995, 90: 34.003, 95: 34.012, 100: 34.018}
+        self.salinity = []
+        for h in self.height_data:
+            for key, value in self.salinity_woa.items():
+                if abs(h-key) <= 2.5:
+                    self.salinity.append(value)
+                    break
+        self.salinity = np.array(self.salinity)
+        self.p_density = []
+    def show_salinity_profile(self):
+        keys = np.array(list(self.salinity_woa.keys()))
+        values = np.array(list(self.salinity_woa.values()))
+        plt.gca().invert_yaxis()
+        plt.plot(values, keys)
+        plt.title("Профиль солености")
+        plt.xlabel('S, %')
+        plt.ylabel('z, м')
+        plt.show()
 
     def show_temp(self):
         fig, ax = plt.subplots(constrained_layout=True)
@@ -37,15 +56,17 @@ class SeaPoint:
         plt.gca().invert_yaxis()
         Z = self.temp_data
         cs = ax.contourf(X, Y, Z, cmap=plt.cm.jet)
+
+        # add continuous color bar
         norm = matplotlib.colors.Normalize(vmin=cs.cvalues.min(), vmax=cs.cvalues.max())
         sm = plt.cm.ScalarMappable(norm=norm, cmap=cs.cmap)
         sm.set_array([])
         cbar = fig.colorbar(sm, ticks=cs.levels)
-        #cbar = fig.colorbar(cs)
         cbar.ax.set_ylabel("температура, С")
+
         ax.set_title("Поле температуры")
         ax.set_xlabel("t, дни")
-        ax.set_ylabel("h, м")
+        ax.set_ylabel("z, м")
         plt.show()
 
     def show_potential_density(self):
@@ -59,19 +80,24 @@ class SeaPoint:
         pressure = gsw.p_from_z(self.height_data, self.latitude)
         absolute_salinity = gsw.SA_from_SP(self.salinity, pressure, self.longitude, self.latitude)
         # приведем аболютную соленость к такому же виду, в каком лежит температура (была ошибка)
-        abs_sal_arr = np.zeros((36, 120961))
-        for i in range(0, 36, 1):
+        abs_sal_arr = np.zeros((len(self.height_data), 120961))
+        for i in range(0, len(self.height_data), 1):
             abs_sal_arr[i, :] = absolute_salinity[i]
-        density_data = gsw.density.sigma0(abs_sal_arr, self.temp_data)
-        CS = ax.contourf(X, Y, density_data, cmap=plt.cm.CMRmap_r)
-        cbar = fig.colorbar(CS)
+        density_data = gsw.density.sigma0(abs_sal_arr, self.temp_data) + 1000
+        cs = ax.contourf(X, Y, density_data, cmap=plt.cm.jet)
+        norm = matplotlib.colors.Normalize(vmin=cs.cvalues.min(), vmax=cs.cvalues.max())
+        sm = plt.cm.ScalarMappable(norm=norm, cmap=cs.cmap)
+        sm.set_array([])
+        cbar = fig.colorbar(sm, ticks=cs.levels)
         cbar.ax.set_ylabel("плотность, кг/м^3")
         ax.set_title("Потенциальная плотность")
         ax.set_xlabel("t, дни")
-        ax.set_ylabel("h, м")
+        ax.set_ylabel("z, м")
         plt.show()
 
 
-point = SeaPoint('/home/anna/PyProjects/diploma/data/S09_10sec.mat')
-point.show_temp()
-# point.show_potential_density()
+point = SeaPoint('/home/anna/PyProjects/diploma/data/S04_10sec.mat')
+# point.show_temp()
+# point.show_salinity_profile()
+point.show_potential_density()
+point.show_frequency()
